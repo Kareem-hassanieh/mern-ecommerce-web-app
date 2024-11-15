771afff34c8b367cd424db0920b0775dee6a5af5
Kareem-hassanieh <kareemalh19@gmail.com>
2024-11-15 21:54:09 +0200

    adding the user model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from 'mongoose';
+
+// Define the User schema
+const UserSchema = new Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    email: {
+        required: true,
+        type: String,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);  // Email regex validation
+            },
+            message: 'Invalid email address.'
+        }
+    },
+    password: {
+        required: true,
+        type: String,
+        minlength: 8,
+        validate: {
+            validator: function (v) {
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(v);  // Password regex validation
+            },
+            message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+        }
+    },
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    }
+});
+
+// Create and export the User model
+export const User = mongoose.model('User', UserSchema);
