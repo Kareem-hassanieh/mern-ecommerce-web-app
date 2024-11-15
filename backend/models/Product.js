c92978b899a444a1d53be71dd2ce761e0e4dfd63
Kareem-hassanieh <kareemalh19@gmail.com>
2024-11-16 00:33:56 +0200

    adding the Product model

---
+++ backend/models/Product.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose'); // Import mongoose
+const { Schema } = mongoose; // Destructure Schema from mongoose
+
+// Define the Product schema
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    pictures: {
+        type: [String],
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    number_of_reviews: {
+        type: Number,
+        required: true
+    },
+    sum_of_ratings: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    }
+});
+
+// Create and export the Product model
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = { Product };
