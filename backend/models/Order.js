92c34272b20c013d739ee1bfc3e5c7504968884f
Kareem-hassanieh <kareemalh19@gmail.com>
2024-11-16 08:49:08 +0200

    creating the order model

---
+++ backend/models/Order.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const OrderSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  items: [
+    {
+      product: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true, 
+      },
+      quantity: {
+        type: Number,
+        required: true, 
+      },
+      price: {
+        type: Number,
+        required: true, 
+      },
+    },
+  ],
+  totalAmount: {
+    type: Number,
+    required: true, 
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Shipped', 'Delivered', 'Cancelled'],
+    default: 'Pending', 
+  },
+  shippingAddress: {
+    type: String,
+    required: true, // 
+  },
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  }
+});
+
+
+const Order = mongoose.model('Order', OrderSchema);
+module.exports = Order;
