da2d0e3f25b6d2a9eebfe7c022a766ad082f1d47
Kareem-hassanieh <kareemalh19@gmail.com>
2024-11-22 01:35:45 +0200

    adding the form component and adding styling to it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,16 @@ import ProductsGallery from "./components/ProductsGallery";
 import AddProductForm from "./components/AddProductForm";
 
 import Header from "./components/Header";
+import Login from "./components/Login";
 
 
 function App() {
   return (
     <>
-    <Header />
+    <Login/>
+    {/* <Header /> */}
       {/* <ProductsGallery /> */}
-      <AddProductForm />
+      {/* <AddProductForm /> */}
     </>
   );
 }
